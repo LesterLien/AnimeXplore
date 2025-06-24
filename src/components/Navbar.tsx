b0ae1e8767a9cb6ba2f91276f83582290423da4c
Lester Lien <lesterlien11@gmail.com>
2025-06-23 20:22:36 -0700

    Added navigator, home, and some few changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div className="h-20 grid grid-cols-3 items-center outline-1 outline-white">
-      <div className="flex justify-start ml-5">
+    <div className="h-20 grid grid-cols-3 items-center bg-[#242424] text-[20px] outline-1">
+      <div className="flex justify-start ml-5 space-x-8">
+        <span>AnimeXplore</span>
+        <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
+        <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
+        <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
       </div>
-      <div className="flex justify-center space-x-8 text-white text-[20px] m-4">
-        <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
-        <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
-        <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
+      <div className="flex justify-end mr-0">
+        Search bar
       </div>
     </div>
   )
