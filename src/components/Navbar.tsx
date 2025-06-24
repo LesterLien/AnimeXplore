fa2d7c02157f5eac5cd04e81f080d6db77c384cb
LesterLien <lesterlien11@gmail.com>
2025-06-23 21:03:28 -0700

    Updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom';
+import { IoIosSearch } from "react-icons/io";
 
 export default function Navbar() {
   return (
-    <div className="h-20 grid grid-cols-3 items-center bg-[#242424] text-[20px] outline-1">
-      <div className="flex justify-start ml-5 space-x-8">
+    <div className="h-20 flex items-center justify-between bg-[#242424] text-[20px] outline-1">
+      <div className="ml-5 space-x-8">
         <span>AnimeXplore</span>
         <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
         <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
         <Link to="/" className="hover:text-[#854CE6] transition-colors duration-200">placeholder</Link>
       </div>
-      <div className="flex justify-end mr-0">
-        Search bar
+      <div className='mr-5 w-50 bg-[#6f7070] p-1 flex flex-row items-center gap-2 rounded-lg'>
+        <IoIosSearch className='text-[22px]'/>
+        <span className='text-[13px]'>Search here...</span>
       </div>
     </div>
   )
