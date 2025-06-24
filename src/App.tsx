85a2c987ca14d3447766e97fbcdc7342e401acfd
LesterLien <lesterlien11@gmail.com>
2025-06-23 21:57:59 -0700

    Added .env and navigator

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styling/index.css'
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar.tsx';
-
+import Navbar from './components/Navbar';
+import Navigator from './components/Navigator';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
         <Navbar/>
+        <Navigator/>
     </BrowserRouter>
   </StrictMode>
 )
