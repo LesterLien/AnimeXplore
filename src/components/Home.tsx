f6280f9e739916b5060f7c03999aaa7a35a3c306
Lester Lien <lesterlien11@gmail.com>
2025-06-27 01:29:50 -0700

    Setting up the initial layout of the home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,43 +1,79 @@
-import { useEffect, useState } from "react"
-import axios from "axios";
+// import { useEffect, useState } from "react"
+// import axios from "axios";
+import { GrNext, GrPrevious } from "react-icons/gr";
 
-type Anime = {
-  images: string;
-  title: string;
-  type: string;
-  episodes: number;
-};
+// type Anime = {
+//   images: string;
+//   title: string;
+//   type: string;
+//   episodes: number;
+// };
 
 export default function Home() {
-  const [topAiringAnime, setTopAiringAnime] = useState<Anime[]>([]);
+//   const [topAiringAnime, setTopAiringAnime] = useState<Anime[]>([]);
+//   const [topUpcomingAnime, setTopUpcomingAnime] = useState<Anime[]>([]);
+//   const [topPopularityAnime, setTopPopularityAnime] = useState<Anime[]>([]);
+//   const [topFavoriteAnime, setTopFavoriteAnime] = useState<Anime[]>([]);
 
 
-  useEffect(() => {
-    const fetchAnime = async () => {
-      try {
-        const response = await axios.get<{ airingTop: Anime[] }>('http://localhost:4000/home');
-        setTopAiringAnime(response.data.airingTop); 
-      } catch (error) {
-        console.error("Error fetching anime:", error);
-      }
-    };
+//   useEffect(() => {
+//     const fetchAnime = async () => {
+//       try {
+//         const response = await axios.get<{ airingTop: Anime[], upcomingTop: Anime[], popularityTop: Anime[], favoriteTop: Anime[] }>('http://localhost:4000/home');
+//         setTopAiringAnime(response.data.airingTop); 
+//         setTopUpcomingAnime(response.data.upcomingTop);
+//         setTopPopularityAnime(response.data.popularityTop); 
+//         setTopFavoriteAnime(response.data.favoriteTop);
+//       } catch (error) {
+//         console.error("Error fetching anime:", error);
+//       }
+//     };
 
-    fetchAnime();
-  }, []);
+//     fetchAnime();
+//   }, []);
 
   return (
-    <div>
-      <span>Top Airing</span>
-      {topAiringAnime.map((anime, index) => (
-        <div key={index}> 
-          <img src={anime.images} className="w-[150px]"/>
-          <div className="flex flex-col ">
-            <span>{anime.title ?? "N/A"}</span>
-            <span>{anime.type ?? "N/A"}</span>
-            <span>{anime.episodes ?? "N/A"}</span>
-          </div>
+    <div className="h-screen">
+        <div className="outline-amber-50 outline-1 w-full h-[60%] flex items-center justify-between pr-5 pl-5 text-3xl">
+            <GrPrevious className="hover:text-[#854CE6] transition-colors duration-200 cursor-pointer"/>
+            <div className="flex flex-col h-full items-center">
+                <span>Picture</span>
+                <div className="flex flex-row gap-x-3 mt-auto mb-5">
+                    <button className="bg-gray-300 size-[10px] rounded-sm">
+
+                    </button>
+                    <button className="bg-gray-300 size-[10px] rounded-sm">
+
+                    </button>
+                    <button className="bg-gray-300 size-[10px] rounded-sm">
+
+                    </button>
+                    <button className="bg-gray-300 size-[10px] rounded-sm">
+
+                    </button>
+                    <button className="bg-gray-300 size-[10px] rounded-sm">
+
+                    </button>
+                </div>
+            </div>
+            <GrNext className="hover:text-[#854CE6] transition-colors duration-200 cursor-pointer"/>
         </div>
-      ))}
+        <div className="outline-amber-400 outline-1 h-[70%] flex justify-center p-5">
+            <div className="h-[50px] outline flex flex-row justify-center items-center p-5 gap-x-5 text-[20px] font-bold">
+                <div>
+                    Top Airing
+                </div>
+                <div>
+                    Top Upcoming
+                </div>
+                <div>
+                    Top Popularity
+                </div>
+                <div>
+                    Top Favorites
+                </div>
+            </div>
+        </div>
     </div>
 
   )
